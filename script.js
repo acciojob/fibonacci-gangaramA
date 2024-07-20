295cecb0a5687bd7c5fce2d04a34337a5b58bbb3
gangaramA <150507677+gangaramA@users.noreply.github.com>
2024-07-20 10:59:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 function fibonacci(num) {
-    if (num === 0) return 0;
-    if (num === 1) return 1;
+    if (num === 1) return 0;
+    if (num === 2) return 1;
 
     let a = 0;
     let b = 1;
 
-    for (let i = 2; i <= num; i++) {
+    for (let i = 3; i <= num; i++) {
         let temp = a + b;
         a = b;
         b = temp;
